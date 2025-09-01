2ff0c8e3a4c48033afabc54511eed60fb88cedcc
Lucas xryz <lucas.deudon04@gmail.com>
2025-09-01 23:39:39 +0200

    AJOUT ELECTRON AVEC LE GIT IGNORE BABY CA MARCHE

---
+++ main.js
@@ -0,0 +1,27 @@
+const { app, BrowserWindow } = require('electron');
+
+function createWindow () {
+  const win = new BrowserWindow({
+    width: 1280,       // largeur de la fenêtre
+    height: 720,       // hauteur de la fenêtre
+    webPreferences: {
+      nodeIntegration: true,
+      contextIsolation: false
+    }
+  });
+
+  win.loadFile('components/runes/spawn first/runes640x360_c1.html');  // ton fichier principal
+  win.setFullScreen(true);  // décommente si tu veux le plein écran dès le départ
+}
+
+app.whenReady().then(() => {
+  createWindow();
+
+  app.on('activate', () => {
+    if (BrowserWindow.getAllWindows().length === 0) createWindow();
+  });
+});
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') app.quit();
+});
